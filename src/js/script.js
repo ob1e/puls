c8bf2332df9962cafa9966b80853d30e14b1f323
Sainchuk <sainchuk.kolia@gmail.com>
2020-05-17 21:43:32 +0300

    add js

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({      
+        speed: 1200,        
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"> <img src="icon/arrow_left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icon/arrow_right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                  dots: true,
+                  arrows: false
+                }
+            }
+
+        ]
+
+      });
+});
