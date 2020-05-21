915a3ad6c963b87a94881af105057cdb5895f4ec
Sainchuk <sainchuk.kolia@gmail.com>
2020-05-21 13:30:49 +0300

    add modals

--- src/js/script.js
+++ src/js/script.js
@@ -38,4 +38,22 @@ $(document).ready(function(){
           $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
         })
       })
+
+      // Modal
+      $('[data-modal="consultation"]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+      });
+      $('.modal__close').on('click', function(){
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+      });
+      // $('.button_mini').on('click', function() {
+      //   $('.overlay, #order').fadeIn('slow');
+      // });
+
+      $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+          $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+          $('.overlay, #order').fadeIn('slow');
+        })
+      });
 });
